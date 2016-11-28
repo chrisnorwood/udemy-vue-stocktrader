aa8ed72fcd46ee735662fa1f396be8b3e8e0d409
Chris Norwood <cnorwood92@gmail.com>
2016-11-27 19:37:38 -0800

    Init repository in finished state

---
+++ src/store/store.js
@@ -0,0 +1,17 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+
+Vue.use(Vuex);
+
+import stocks from './modules/stocks';
+import portfolio from './modules/portfolio';
+
+import * as actions from './actions';
+
+export const store = new Vuex.Store({
+  actions,
+  modules: {
+    stocks,
+    portfolio,
+  },
+});
